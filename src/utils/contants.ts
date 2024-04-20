0e20c1e20f216484bfc11a2c2050c0647059e745
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2024-04-20 23:23:11 +0530

    logo path correction

--- src/utils/contants.ts
+++ src/utils/contants.ts
@@ -7,7 +7,6 @@ export const METADATA: Metadata = {
   description:
     "Transform the way your business operates with our cutting-edge enterprise SaaS solutions that streamline processes, increases efficiency, and drives growth, all while providing top-notch security and support.",
   keywords: "Nurturing Talent, Crafting Excellence, Delivering Innovation",
-  icons: LOGO_URL,
   openGraph: {
     title: "Trisandya Technology Solutions",
     description:
